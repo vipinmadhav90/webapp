356a29749fa4135f70d73fae3a9c2c40b1b94407
Vipin <vipinmadhav90@gmail.com>
2018-12-17 10:49:02 +0000

    working code

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, url_for
+import MySQLdb
+db = MySQLdb.connect("localhost","dbuser","1qazXSW21@","webapp")
+cursor = db.cursor()
+
 app = Flask(__name__)
+app.secret_key = "super secret key123"
 
 @app.route('/')
 def index():
@@ -9,7 +14,10 @@ def index():
 def submit():
    if request.method == 'POST':
        message = request.form['message']
-       return render_template('success.html', message=message)
+       cursor.execute("INSERT INTO posts (post) VALUES(%s)", (message,))
+       db.commit()
+       flash('Your message saved successfully')
+       return render_template('result.html')
    else:
        pass
 
@@ -17,9 +25,23 @@ def submit():
 def search():
    if request.method == 'POST':
        message = request.form['message']
-       return render_template('result.html', message=message)
+       x = cursor.execute("SELECT * FROM posts WHERE post = (%s)",(message,))
+       print(x)
+       if int(x) > 0:
+	    flash('Your message is already existing!')
+       else:
+	    flash('Your message deosnt existing!')
+       return render_template('result.html')
    else:
        pass
 
+@app.route('/ListMessage',  methods=['POST'])
+def list():
+       cursor.execute("SELECT * FROM posts")
+       data = cursor.fetchall()
+       for raw in data:
+	    flash(raw[1])
+       return render_template('result.html')
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8080)
+    app.run(debug = True, host='0.0.0.0',port=8080)
