4d90eb23f22cc0eeef4e21a77f5b3f1ade885a1b
vipinmadhav90 <34733186+vipinmadhav90@users.noreply.github.com>
2018-12-18 22:20:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,11 @@ password = os.environ['APP_DB_PASSWORD']
 db = MySQLdb.connect(db_host,username,password,database)
 cursor = db.cursor()
 cursor.execute("CREATE TABLE IF NOT EXISTS posts (post_id INT AUTO_INCREMENT, post VARCHAR(255 ), PRIMARY KEY(post_id)) ENGINE = InnoDB;")
-
+cursor.close() 
+db.close()
+	
 app = Flask(__name__)
-app.secret_key = "super secret key123"
+app.secret_key = "super secret key1234"
 
 @app.route('/')
 def index():
@@ -20,8 +22,12 @@ def index():
 def submit():
    if request.method == 'POST':
        message = request.form['message']
+       db = MySQLdb.connect(db_host,username,password,database)
+       cursor = db.cursor()
        cursor.execute("INSERT INTO posts (post) VALUES(%s)", (message,))
        db.commit()
+       cursor.close() 
+       db.close()
        flash('Your message saved successfully')
        return render_template('result.html')
    else:
@@ -31,7 +37,11 @@ def submit():
 def search():
    if request.method == 'POST':
        message = request.form['message']
+       db = MySQLdb.connect(db_host,username,password,database)
+       cursor = db.cursor()
        x = cursor.execute("SELECT * FROM posts WHERE post = (%s)",(message,))
+       cursor.close() 
+       db.close()
        if int(x) > 0:
 	    flash('Your message is existing!')
             if message == message[::-1]:
@@ -46,8 +56,12 @@ def search():
 
 @app.route('/ListMessage',  methods=['POST'])
 def list():
+       db = MySQLdb.connect(db_host,username,password,database)
+       cursor = db.cursor()
        cursor.execute("SELECT * FROM posts")
        data = cursor.fetchall()
+       cursor.close() 
+       db.close()
        flash('Following are the mssages found in database:')
        for raw in data:
 	    flash(raw[1])
@@ -57,8 +71,12 @@ def list():
 def delete():
    if request.method == 'POST':
        message = request.form['message']
+       db = MySQLdb.connect(db_host,username,password,database)
+       cursor = db.cursor()
        x = cursor.execute("DELETE FROM posts WHERE post = (%s)",(message,))
        db.commit()
+       cursor.close() 
+       db.close()
        if int(x) == 0:
             flash('Your message deosnt exist in database')
        else:
